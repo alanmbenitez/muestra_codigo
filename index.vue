d70419e815b64774570561843f6d37ea0ab5b7c0
Alan Benitez <49704791+alanmbenitez@users.noreply.github.com>
2021-03-04 11:19:14 -0300

    Add files via upload

--- index.vue
+++ index.vue
@@ -1,99 +1,178 @@
 <template>
     <CustomMask :show="isShowPage">
         <main>
-            <div class="schools__main_container">
-                <!-- ------------------------------Hero---------------------------------- -->
-                <Hero
-                    :title="$t('colegios.title')"
-                    :detail="$t('colegios.detail_hero')"
-                    :image-u-r-l="$t('colegios.img_hero')"
-                    :is-course="false"
-                    :one-button="true"
-                    :boton="$t('colegios.boton_hero')"
+            <div class="bg-dh-gray">
+                <!-- ---------------------------HERO------------------ -->
+                <Hero :title="$t('empresas.title')" :is-course="false" :one-button="false" />
+                <!-- ----------------------- -CONTACTO---------------------------------------------------------- -->
+
+                <Contacto
+                    :title="$t('empresas.subtitle')"
+                    :email="$t('empresas.email')"
+                    :telefono="$t('empresas.tel')"
                 />
-                <!-- ------------------------- texto  ---------------------------------- -->
-                <Texto :texto="$t('colegios.text_pre')" />
+                <!-- - --------------------------- BANNER    -------------------------- -->
 
-                <!-- ------------------------- avalado por ---------------------------------- -->
-                <Aval :texto="$t('colegios.cert')" :href="$t('colegios.pdf_cert')" :src="$t('colegios.logo_cert')" />
-
-                <!-- -------------------------- video  ------------------------------------->
-                <VideoIntro
-                    :url="'tqQGe7xdb0E'"
-                    :comment="'Juana, 11 años. Florida Day School'"
-                    :detail="titulo_video"
+                <Banner
+                    :title="$t('empresas.title_txt')"
+                    :texto="$t('empresas.texto_parrafo')"
+                    :boton="$t('empresas.btn_parrafo')"
                 />
+                <!--  ---------------------------------- hero programas -------------------------------------- -->
+                <Hero :title="$t('empresas.banner_medio')" :is-course="false" :one-button="false" />
+                <!-- --------------------------------------PROGRAMAS ------------------------------- -->
 
-                <!-- ------------------- metodologias de enseñanza------------------------ -->
-                <WhyCourse
-                    :cards="$t('colegios.cuadros')"
-                    :title="$t('colegios.titulo_cards')"
-                    cardHeight="h-12rem md:h-auto"
-                />
+                <Programas :programas="$t('empresas.cards')" />
 
-                <!-- ---------------------------opiniones directivos ------------------------- -->
+                <!--------------------------------------CARDS---------------------------------- -->
+                <WhyCourse :cards="$t('empresas.cuadros')" :title="$t('empresas.title_card')" />
+                <!-- ------------------------------------------------------------- -->
+                <!-- <VueSlickCarousel
+                v-if="empresas.opiniones && empresas.opiniones.length > 0"
+                ref="carousel"
+                v-bind="carousel_settings"
+            > -->
                 <Opinion
-                    :src="$t('colegios.foto_referente')"
-                    :nombre_img="$t('colegios.nombre_referente')"
-                    :texto="$t('colegios.opinion')"
-                    :referente="$t('colegios.referente')"
+                    v-for="(opinion, index) in empresas.opiniones"
+                    :key="index"
+                    :src="opinion.foto_referente"
+                    :nombre_img="opinion.nombre_referente"
+                    :texto="opinion.opinion"
+                    :referente="opinion.referente"
                 />
-
-                <!-- ----------------------------proyecto alumnos ------------------------------- -->
-                <Proyectos :proyectos="$t('colegios.proyectos')" :texto="$t('colegios.title_proyectos')" />
-
-                <!-- ------------------------------logos instituciones---------------------------- -->
-                <Logos :texto="$t('colegios.titulo_logos')" :logos="$t('colegios.logos')" />
-
-                <!-- ---------------------------- progreso------------------------------------------ -->
-                <Timeline :progreso="$t('colegios.progreso')" />
-
-                <!-- ------------------------------FAQs--------------------------------------------- -->
-                <FAQs :preguntas="$t('colegios.faqs')" :title="$t('colegios.title_faqs')" />
+                <!--       <template #customPaging="page">
+                    <div class="custom-dot lg:hidden">
+                        {{ page }}
+                    </div>
+                </template>
+            </VueSlickCarousel> -->
+                <!-- ---------------------------------   LOGOS------------------------------------- -->
+                <Logos :texto="$t('empresas.titulo_logos')" :logos="$t('empresas.logos')" :color="color" />
+                <!-- ------------------------Videos -------------------------------- -->
+                <Videos :all-videos="empresas.videos" />
             </div>
         </main>
     </CustomMask>
 </template>
+
 <script>
+    import { mapState } from 'vuex';
     import CustomMask from '~/plugins/Mask.vue';
+    import deadpool from '@/mixins/deadpool';
 
     export default {
-        name: 'Colegios',
+        name: 'Empresas',
         scrollToTop: true,
         components: {
-            Hero: () => import('~/components/colegios/Hero.vue'),
-            VideoIntro: () => import('~/components/cursos/VideoIntro.vue'),
-            WhyCourse: () => import('~/components/cursos/WhyCourse.vue'),
-            FAQs: () => import('~/components/faq/Faqs.vue'),
-            Texto: () => import('~/components/colegios/Text_ctrl.vue'),
-            Aval: () => import('~/components/colegios/Aval.vue'),
+            Hero: () => import('~/components/general/HeroGral.vue'),
             Opinion: () => import('~/components/colegios/Opinion.vue'),
-            Proyectos: () => import('~/components/colegios/Proyectos.vue'),
+            WhyCourse: () => import('~/components/cursos/WhyCourse.vue'),
             Logos: () => import('~/components/colegios/Logos.vue'),
-            Timeline: () => import('~/components/colegios/Timeline'),
+            Programas: () => import('~/components/empresas/Programas.vue'),
+            Contacto: () => import('~/components/empresas/BannerContacto.vue'),
+            Banner: () => import('~/components/empresas/Banner.vue'),
+            Videos: () => import('~/components/empresas/Videos.vue'),
             CustomMask,
         },
+        mixins: [deadpool],
         data() {
             return {
                 isShowPage: false,
-                titulo_video:
-                    '"Me gustó porque me enseñaron a hacer páginas web, que es algo que siempre quise hacer. Además las clases fueron interesantes y divertidas"',
+                color: 'bg-dh-gray',
+                carousel_settings: {
+                    centerMode: true,
+                    centerPadding: '10px',
+                    dots: true,
+                    arrows: false,
+                    lazyLoad: 'ondemand',
+                    cssEase: 'ease-in-out',
+                    autoplay: true,
+                    infinite: true,
+                    swipeToSlide: true,
+                    dotsClass: 'dots-parent',
+                    slidesToShow: 300,
+                    responsive: [
+                        {
+                            breakpoint: 20000,
+                            settings: 'unslick',
+                        },
+                        {
+                            breakpoint: 959,
+                            settings: {
+                                slidesToShow: 2,
+                            },
+                        },
+                        {
+                            breakpoint: 639,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                            },
+                        },
+                    ],
+                },
+                metas: {
+                    title: `${this.$t('business.meta.title')} || Digital House`,
+                    description: this.$t('business.meta.description'),
+                    src: '',
+                },
             };
+        },
+        computed: {
+            ...mapState({
+                empresas: (state) => state.empresas,
+            }),
         },
         created() {
             setTimeout(() => {
                 this.isShowPage = true;
             }, 100);
         },
+        head() {
+            return {
+                title: this.metas.title,
+                meta: this.metaMetas(this.metas),
+                link: [
+                    {
+                        rel: 'canonical',
+                        href: `https://www.digitalhouse.com/${this.$env('APP_COUNTRY')}/${this.$t('routes.business')}`,
+                    },
+                    {
+                        rel: 'alternate',
+                        href: `https://www.digitalhouse.com/latam/${this.$t('routes.business', 'es-AR')}`,
+                        hreflang: 'es',
+                    },
+                    {
+                        rel: 'alternate',
+                        href: `https://www.digitalhouse.com/ar/${this.$t('routes.business', 'es-AR')}`,
+                        hreflang: 'es-AR',
+                    },
+                    {
+                        rel: 'alternate',
+                        href: `https://www.digitalhouse.com/br/${this.$t('routes.business', 'pt-BR')}`,
+                        hreflang: 'pt-BR',
+                    },
+                    {
+                        rel: 'alternate',
+                        href: `https://www.digitalhouse.com/mx/${this.$t('routes.business', 'es-MX')}`,
+                        hreflang: 'es-MX',
+                    },
+                    {
+                        rel: 'alternate',
+                        href: `https://www.digitalhouse.com/${this.$t('routes.business', 'es-AR')}`,
+                        hreflang: 'x-default',
+                    },
+                ],
+            };
+        },
     };
 </script>
-<style scoped>
-    .schools__main_container {
-        @apply bg-dh-gray;
-    }
-</style>
+
 <style>
-    .y-video__inner {
-        padding-bottom: 59.25% !important;
+    .minWidth {
+        min-width: 19rem;
+    }
+    .maxWidth {
+        max-width: 32rem;
     }
 </style>
